989f5159cfe28d89ffd1a12047c57882efddb537
Daniele Espindola <daniele.ssoares@gmail.com>
2025-09-08 11:20:22 -0300

    Ajustes no nó de condições

--- src/app/features/flow/node-condition/node-condition.component.ts
+++ src/app/features/flow/node-condition/node-condition.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input } from '@angular/core';
-import { NgFor } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCodeBranch } from '@fortawesome/free-solid-svg-icons';
 import { GraphNode } from '../graph.types';
@@ -7,7 +6,7 @@ import { GraphNode } from '../graph.types';
 @Component({
   selector: 'app-node-condition',
   standalone: true,
-  imports: [NgFor, FontAwesomeModule],
+  imports: [FontAwesomeModule],
   templateUrl: './node-condition.component.html',
   styleUrl: './node-condition.component.scss'
 })
