d01f9b43e9c877bcbd5c57b1abfba97e7b99f167
Daniele Espindola <daniele.ssoares@gmail.com>
2025-09-08 14:33:41 -0300

    Implementado o score das questões

--- src/app/features/flow/node-question/node-question.component.ts
+++ src/app/features/flow/node-question/node-question.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { TitleCasePipe } from '@angular/common';
+import { NgIf, TitleCasePipe } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faComment, faStar } from '@fortawesome/free-solid-svg-icons';
 import { GraphNode } from '../graph.types';
@@ -7,7 +7,7 @@ import { GraphNode } from '../graph.types';
 @Component({
   selector: 'app-node-question',
   standalone: true,
-  imports: [FontAwesomeModule, TitleCasePipe],
+  imports: [NgIf, FontAwesomeModule, TitleCasePipe],
   templateUrl: './node-question.component.html',
   styleUrl: './node-question.component.scss'
 })
